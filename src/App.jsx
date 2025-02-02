af4da9638852d4ecfb55d0dd6f18ef83729c8b0c
Dome <arillotta@gmail.com>
2025-02-02 19:55:03 +0100

    last commit of the day

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import RecipeDetail from "./pages/RecipeDetail";
+import Favorites from "./pages/Favorites";
+
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/favorites" element={<Favorites />} />
+
       </Routes>
     </Router>
   );
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from "react";
+import FavoritesContext from "../context/FavoritesContext";
+import { Link } from "react-router-dom";
+
+function Favorites() {
+  const { favorites, removeFavorite } = useContext(FavoritesContext);
+
+  return (
+    <div className="container">
+      <h1 className="text-center my-4">❤️ Favorite Recipes</h1>
+      {favorites.length === 0 ? (
+        <p className="text-center">No favorites yet. Add some!</p>
+      ) : (
+        <div className="row">
+          {favorites.map((recipe) => (
+            <div key={recipe.id} className="col-md-4 mb-4">
+              <div className="card">
+                <img src={recipe.image} className="card-img-top" alt={recipe.title} />
+                <div className="card-body">
+                  <h5 className="card-title">{recipe.title}</h5>
+                  <Link to={`/recipe/${recipe.id}`} className="btn btn-primary">View Details</Link>
+                  <button className="btn btn-danger mt-2" onClick={() => removeFavorite(recipe.id)}>
+                    ❌ Remove
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Favorites;
